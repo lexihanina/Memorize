efb3b970aafe28ceaa043faea464e0b46cc3038e
Lexi Hanina <lexihanina@gmail.com>
2022-09-14 14:49:13 +0200

    Initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alex Hanina on 14.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
